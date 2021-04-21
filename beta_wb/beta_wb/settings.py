1e23bd31b0e27395609b98e16a5e51186a7c1447
v.sotnikov <sotenchik7@gmail.com>
2021-04-21 15:12:05 +0300

    init

---
+++ beta_wb/beta_wb/settings.py
@@ -0,0 +1,65 @@
+from shutil import which
+import scrapy
+from scrapy import Request
+from scrapy_selenium import SeleniumRequest
+from selenium import webdriver
+import urllib3
+import requests
+import selenium
+
+
+SELENIUM_DRIVER_NAME = 'chrome'
+SELENIUM_DRIVER_EXECUTABLE_PATH = which(r'C:\Users\v.sotnikov\Desktop\Vlad\Vlad\chromedriver.exe')
+SELENIUM_DRIVER_ARGUMENTS = ['--headless']
+
+
+
+BOT_NAME = 'beta_wb'
+
+SPIDER_MODULES = ['beta_wb.spiders']
+NEWSPIDER_MODULE = 'beta_wb.spiders'
+
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+# USER_AGENT = 'vlad (+http://www.google.com)'
+
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = False
+
+DOWNLOAD_DELAY = 0
+DOWNLOAD_TIMEOUT = 30
+RANDOMIZE_DOWNLOAD_DELAY = True
+
+REACTOR_THREADPOOL_MAXSIZE = 128
+CONCURRENT_REQUESTS = 256
+CONCURRENT_REQUESTS_PER_DOMAIN = 256
+CONCURRENT_REQUESTS_PER_IP = 256
+
+AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_START_DELAY = 1
+AUTOTHROTTLE_MAX_DELAY = 0.25
+AUTOTHROTTLE_TARGET_CONCURRENCY = 128
+AUTOTHROTTLE_DEBUG = True
+
+RETRY_ENABLED = True
+RETRY_TIMES = 3
+RETRY_HTTP_CODES = [500, 502, 503, 504, 400, 401, 403, 404, 405, 406, 407, 408, 409, 410, 429]
+
+PROXY_POOL_ENABLED = True
+
+
+DOWNLOADER_MIDDLEWARES = {
+
+    'scrapy_proxy_pool.middlewares.ProxyPoolMiddleware': 610,
+    'scrapy_proxy_pool.middlewares.BanDetectionMiddleware': 620,
+    'scrapy_selenium.SeleniumMiddleware': 800,
+    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+    'scrapy.spidermiddlewares.referer.RefererMiddleware': 80,
+    'scrapy.downloadermiddlewares.retry.RetryMiddleware': 90,
+    'scrapy_fake_useragent.middleware.RandomUserAgentMiddleware': 120,
+    'scrapy.downloadermiddlewares.cookies.CookiesMiddleware': 130,
+    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+    'scrapy.downloadermiddlewares.redirect.RedirectMiddleware': 900
+
+}
+
---
+++ beta_wb/beta_wb/spiders/beta_wb.py
@@ -0,0 +1,102 @@
+import scrapy
+from scrapy import Request
+from scrapy_selenium import SeleniumRequest
+from selenium import webdriver
+import urllib3
+import selenium
+import requests
+import time
+import re
+import json
+import requests
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import TimeoutException
+from bs4 import BeautifulSoup
+import openpyxl
+import math
+
+class WB(scrapy.Spider):
+    name = 'wb'
+    allowed_domains = ['wildberries.ru']
+    # start_urls = ['https://www.wildberries.ru/catalog/9131176/detail.aspx?targetUrl=GP']
+    global df
+    df = pd.DataFrame(
+        columns=['Артикул', 'Актикул Кари', 'Байер', 'Направление', 'Группа', 'Подгруппа', 'ТМ', 'Название',
+                 'Первая цена', 'Текущая цена', 'Отзывов', '+ к отзывам', 'Купили раз', '+к купили раз',
+                 'Кол-во звезд', 'Поразмерная дистрибуция'])
+    global df_vse
+    df_vse = pd.read_excel(r'C:\Users\v.sotnikov\PycharmProjects\BETA_WB\beta_wb\beta_wb\spiders\isxod\все.xlsx')
+
+    def start_requests(self):
+        global i
+        global df_vse
+        for i in range(len(df_vse)):
+            print(i, 'HERE BITCH')
+            art = df_vse['Артикул'][i]
+            URL_1 = 'https://www.wildberries.ru/catalog/'
+            URL_2 = str(art)
+            URL_3 = '/detail.aspx?targetUrl=GP'
+            global URL
+            URL = URL_1 + URL_2 + URL_3
+            yield SeleniumRequest( url=URL,callback=self.parse, cb_kwargs={'index':i})
+            # if i==5:
+            #     # df.to_excel('all_all.xlsx', index=False)
+            #     break
+
+    def parse(self,response,index):
+        global i
+        global df
+        count_able=0
+        count_disable=0
+        driver = response.request.meta['driver']
+        # import ipdb; ipdb.set_trace()
+        # time.sleep(1)
+        # print(driver.page_source)
+        # driver.find_element_by_class_name('order-quantity j-orders-count-wrapper').extract()
+        for k in response.selector.xpath('//label[@class="j-size active"]/span[1]/text()'):
+            print(k.get())
+            count_able = count_able + 1
+        for k in response.selector.xpath('//label[@class="j-size"]/span[1]/text()'):
+            print(k.get())
+            count_able = count_able + 1
+        for kk in response.selector.xpath('//label[@class="j-size disabled"]/span[1]/text()'):
+            print(kk.get())
+            count_disable = count_disable + 1
+        if count_able!=0:
+            distribution = round((count_able/(count_able+count_disable))*100)
+        else:
+            distribution = 0
+        print(index, '_________',index)
+        df = df.append( {'Артикул' : df_vse['Артикул'].iloc[index],
+            'Направление' : df_vse['Направление'].iloc[index],
+            'Группа': df_vse['Категория'].iloc[index],
+            'Подгруппа': df_vse['Подкатегория'].iloc[index],
+            'Первая цена' : re.sub('[\[\'\]]','',str(response.selector.xpath("//*[@id='container']/div[1]/div[2]/div[4]/div[2]/div/div/span/del/text()").extract()).replace(' ','').replace('\\xa0', '').replace('₽','')),
+            'Текущая цена' : re.sub('[\[\'\]]','',str(response.selector.xpath("//*[@id='container']/div[1]/div[2]/div[4]/div[2]/div/div/div/span/text()").extract()).replace(' ','').replace('\\xa0', '').replace('₽','')),
+            'Отзывов' : str(re.sub('\D', '',str(response.selector.xpath("//*[@id='a-Comments']/text()").extract()))),
+            'ТМ' : re.sub('[\[\'\]]','',str(response.selector.xpath("//*[@id='container']/div[1]/div[2]/div[1]/div[1]/span[1]/text()").extract()).strip().replace('[','')),
+            'Купили раз' : str(re.sub('\D', '',str(response.selector.xpath("//p[@class='order-quantity j-orders-count-wrapper']/span[1]/text()").extract()))),
+            'Название' : re.sub('[\[\'\]]','',str(response.selector.xpath('//*[@id="container"]/div[1]/div[2]/div[1]/div[1]/span[2]/text()').extract())),
+            'Кол-во звезд' : re.sub('[\[\'\]]','',str(response.selector.xpath('//*[@id="container"]/div[1]/div[2]/div[2]/div[2]/p/span/text()').extract())),
+            'Поразмерная дистрибуция':distribution },ignore_index=True )
+        if index==10000:
+            df.to_excel('10000.xlsx', index=False)
+        if index == 20000:
+            df.to_excel('20000.xlsx', index=False)
+        if index==len(df_vse):
+            df.to_excel('all_all.xlsx', index=False)
+
+
+
+
+
+
+
+
